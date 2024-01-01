16980b99e4b726fc350f664b44e04637d98d70c4
Sree0211 <sree0050@gmail.com>
2024-01-01 20:14:42 +0100

    Added Pause/Resume widget and Progressbar

--- ObjectTracking_Counting.py
+++ ObjectTracking_Counting.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, QTimer
 
 import numpy as np
 import cv2
@@ -6,43 +6,103 @@ import time
 
 class VideoTracking(QThread):
 
+    # Required Signals to connect with the GUI
+    # videoEnded = pyqtSignal()
+
     maskPixmap = pyqtSignal(np.ndarray)
     framePixmap = pyqtSignal(np.ndarray)
+    video_length_signal = pyqtSignal(int)
+    curr_signal = pyqtSignal(int)
 
-
-    def __init__(self):
+    def __init__(self, video_path):
         super().__init__()
         self._run_flag = True
-    
+        self._paused = False
+        self.cap = cv2.VideoCapture(video_path)
+        self.total_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.frame_rate = int(self.cap.get(cv2.CAP_PROP_FPS))
+        self.current_frame = 0
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_frame)
+
     def run(self):
-        cap = cv2.VideoCapture("/Users/sreenathswaminathan/Desktop/Uni-Docs/Autonomous fahren kurs/CV Project/Object-Detection-and-Tracking/pexels_videos_1171461 (1080p).mp4")
-        object_detector = cv2.createBackgroundSubtractorMOG2(history=100,varThreshold=60)
+        object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=60)
 
-        while self._run_flag:
-            ret, frame = cap.read()
-            # height, width, _ = frame.shape
+        while self._run_flag and self.current_frame < self.total_frames:
+            if not self._paused:
+                ret, frame = self.cap.read()
+                if ret:
+                    roi = frame[200:1000, 15:800]
+                    mask = object_detector.apply(roi)
+                    _, mask = cv2.threshold(mask, 253, 255, cv2.THRESH_BINARY)
 
-            roi = frame[200:1000, 15:800]
-            mask = object_detector.apply(roi)
-            _, mask = cv2.threshold(mask, 253, 255, cv2.THRESH_BINARY)
-            
-            contours, _ = cv2.findContours(mask,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            for cnt in contours:
-                area = cv2.contourArea(cnt)
-                if area > 100:
-                    cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 2)
-                    x, y, w, h = cv2.boundingRect(cnt)
-                    cv2.rectangle(roi, (x, y), (x+w, y+h), (0, 255, 0), 3)
-            
-            if ret:
-                self.framePixmap.emit(frame)
-                self.maskPixmap.emit(mask)
+                    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-            time.sleep(0.033)
-            
-        cap.release()
+                    for cnt in contours:
+                        
+                        area = cv2.contourArea(cnt)
+                        # Perform Non-Maximum Suppression (NMS)
+                        cnt = self.non_max_suppression(cnt, 0.5)
+                        if area > 100:
+
+                            cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 2)
+                            x, y, w, h = cv2.boundingRect(cnt)
+                            cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+                    self.maskPixmap.emit(mask)
+                    self.framePixmap.emit(frame)
+                    self.video_length_signal.emit(self.total_frames)
+                    self.curr_signal.emit(self.current_frame)
+
+                    self.current_frame += 1
+                
+                time.sleep(0.033)
+
+        self.cap.release()
+
+    def pause(self):
+        self.timer.stop()
+        self._paused = True
+
+    def resume(self):
+        self.timer.start(1000 / self.frame_rate)
+        self._paused = False
 
     def stop(self):
-        """Sets run flag to False and waits for thread to finish"""
         self._run_flag = False
-        self.wait()
+
+    def update_frame(self):
+        self.timer.stop()
+        self.resume()
+
+    def non_max_suppression(self,boxes, overlap_thresh):
+        if len(boxes) == 0:
+            return []
+
+        pick = []
+
+        x1 = boxes[:, 0]
+        y1 = boxes[:, 1]
+        x2 = boxes[:, 2]
+        y2 = boxes[:, 3]
+        area = (x2 - x1 + 1) * (y2 - y1 + 1)
+        idxs = np.argsort(boxes[:, 4])
+
+        while len(idxs) > 0:
+            last = len(idxs) - 1
+            i = idxs[last]
+            pick.append(i)
+
+            xx1 = np.maximum(x1[i], x1[idxs[:last]])
+            yy1 = np.maximum(y1[i], y1[idxs[:last]])
+            xx2 = np.minimum(x2[i], x2[idxs[:last]])
+            yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+            w = np.maximum(0, xx2 - xx1 + 1)
+            h = np.maximum(0, yy2 - yy1 + 1)
+
+            overlap = (w * h) / area[idxs[:last]]
+
+            idxs = np.delete(idxs, np.concatenate(([last], np.where(overlap > overlap_thresh)[0])))
+
+        return boxes[pick]
--- ObjectTracking_Window.py
+++ ObjectTracking_Window.py
@@ -1,11 +1,11 @@
 import sys
+from PyQt5 import QtWidgets
 from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtCore import Qt,QThread, pyqtSignal, pyqtSlot, QTimer
-from PyQt5.QtWidgets import QWidget,QApplication, QVBoxLayout, QLabel, QPushButton, QProgressBar
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, QTimer, pyqtSlot
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QLabel, QPushButton, QProgressBar, QToolBar, QMainWindow
 import numpy as np
 import cv2
 from ObjectTracking_Counting import VideoTracking
-
 
 class VideoPanel(QWidget):
     def __init__(self, parent, label_text, geometry):
@@ -13,9 +13,14 @@ class VideoPanel(QWidget):
         self.setGeometry(*geometry)
 
         self.image_label = QLabel(self)
-        self.image_label.resize(800, 600)
+        self.image_label.resize(700, 600)
 
         self.textLabel = QLabel(label_text, self)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.image_label)
+        vbox.addWidget(self.textLabel)
+        self.setLayout(vbox)
 
         self.show()
 
@@ -34,63 +39,74 @@ class VideoPanel(QWidget):
         h, w, ch = rgb_image.shape
         bytes_per_line = ch * w
         convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
-        p = convert_to_Qt_format.scaled(800, 600, Qt.KeepAspectRatio)
+        p = convert_to_Qt_format.scaled(700, 600, Qt.KeepAspectRatio)
         return QPixmap.fromImage(p)
 
-class AppWindow(QWidget):
-    def __init__(self):
+class AppWindow(QMainWindow):
+    def __init__(self, video_path):
         super().__init__()
-        self.init_ui()
 
-    def init_ui(self):
         self.setWindowTitle("Real-time Object Tracking")
-        self.setGeometry(100, 100, 1400, 700)
+        self.setGeometry(100, 100, 1200, 1000)
 
-        self.create_video_panels()
+        # Create Toolbar
+        self.toolbar = self.addToolBar("Tools")
+        self.toolbar.addAction("Action 1")
+        self.toolbar.addAction("Action 2")
 
-        # Creating Pause and Play buttons
-        self.pause_button = QPushButton('Pause')
-        self.pause_button.move(100,100)
-        
-        self.pause_button.clicked.connect(self.pause_video)
-        
+        # Create Video Panels
+        self.video_panel_original = VideoPanel(self, "Object Detection", (20, 50, 700, 600))
+        self.video_panel_masked = VideoPanel(self, "Masked Video for our ROI", (750, 50, 700, 600))
+
+        # Create Pause/Resume Button
+        self.pause_button = QPushButton('Pause', self)
+        self.pause_button.setGeometry(100, 700, 120, 40)
+        self.pause_button.clicked.connect(self.pause_resume_video)
+
+        # Create Progress Bar
+        self.progress_bar = QProgressBar(self)
+        self.progress_bar.setGeometry(250, 700, 500, 40)
+
+        # Activate timer
         self.timer = QTimer(self)
-
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.pause_button)
-
-        self.progress = QProgressBar()
-        vbox.addWidget(self.progress)
-
-        self.setLayout(vbox)
+        
+        # Create Video Thread
+        self.video_thread = VideoTracking(video_path)
+        self.video_thread.framePixmap.connect(self.video_panel_original.update_frame_image)
+        self.video_thread.maskPixmap.connect(self.video_panel_masked.update_mask_image)
+        self.video_thread.video_length_signal.connect(self.setup_progress_bar)
+        self.video_thread.curr_signal.connect(self.update_progress_bar)
+        self.video_thread.start()
 
         self.show()
 
-    def create_video_panels(self):
-        self.video_panel_original = VideoPanel(self, "Object Detection", (0, 0, 800, 800))
-        self.video_panel_masked = VideoPanel(self, "Masked Video", (800, 100, 500, 500))
-
-        self.thread = VideoTracking()
-        self.thread.framePixmap.connect(self.video_panel_original.update_frame_image)
-        self.thread.maskPixmap.connect(self.video_panel_masked.update_mask_image)
-        self.thread.start()
-
-    def pause_video(self):
-        # Resume the video thread again
+    def pause_resume_video(self):
         if self.timer.isActive():
             self.timer.stop()
             self.pause_button.setText('Resume')
+            self.video_thread.pause()
         else:
-            self.timer.timeout.connect()
+            self.timer.start(1000 / self.video_thread.frame_rate)
+            self.pause_button.setText('Pause')
+            self.video_thread.resume()
+
+    def setup_progress_bar(self, total_frames):
+        self.progress_bar.setMaximum(100)
+        self.progress_bar.setValue(0)
+        self.total_frames = total_frames
+
+    def update_progress_bar(self, current_frame):
+        progress = int((current_frame / self.total_frames) * 100)
+        if current_frame == self.total_frames:
+            self.timer.stop()
+        self.progress_bar.setValue(progress)
 
     def closeEvent(self, event):
-        self.thread.stop()
-        event.accept() 
+        self.video_thread.stop()
+        event.accept()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = AppWindow()
-
-    window.show()
+    video_path = "/Users/sreenathswaminathan/Desktop/Uni-Docs/Autonomous fahren kurs/CV Project/Object-Detection-and-Tracking/pexels_videos_1171461 (1080p).mp4"
+    window = AppWindow(video_path)
     sys.exit(app.exec_())
-
